fefa3a32a205e0f9d2e4f6753f9c8d77d9f2690d
varanasirithwik6 <rithwikbblvarun@gmail.com>
2026-05-04 15:53:55 +0530

    UC12: Subtraction and Division Operations on Quantity Measurements

--- QuantityMeasurementApp/src/com/apps/quantitymeasurement/Length.java
+++ QuantityMeasurementApp/src/com/apps/quantitymeasurement/Length.java
@@ -10,31 +10,49 @@ public class Length {
         this.unit = unit;
     }
 
-    private double convertToInches() {
+    // Convert to inches
+    private double toInches() {
         if (unit == LengthUnit.FEET) {
-            return value * 12.0;
-        } else {
-            return value;
+            return value * 12;
         }
+        return value;
     }
 
+    // Convert from inches
+    private double fromInches(double inches, LengthUnit targetUnit) {
+        if (targetUnit == LengthUnit.FEET) {
+            return inches / 12;
+        }
+        return inches;
+    }
+
+    // EQUALS
     @Override
     public boolean equals(Object obj) {
-
-        if (this == obj) {
-            return true;
-        }
-
-        if (obj == null) {
-            return false;
-        }
-
-        if (!(obj instanceof Length)) {
-            return false;
-        }
+        if (this == obj) return true;
+        if (obj == null || !(obj instanceof Length)) return false;
 
         Length other = (Length) obj;
+        return Double.compare(this.toInches(), other.toInches()) == 0;
+    }
 
-        return Double.compare(this.convertToInches(), other.convertToInches()) == 0;
+    // ADD
+    public Length add(Length other) {
+        double result = this.toInches() + other.toInches();
+        return new Length(fromInches(result, this.unit), this.unit);
+    }
+
+    // SUBTRACT
+    public Length subtract(Length other) {
+        double result = this.toInches() - other.toInches();
+        return new Length(fromInches(result, this.unit), this.unit);
+    }
+
+    // DIVIDE
+    public double divide(Length other) {
+        if (other.toInches() == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return this.toInches() / other.toInches();
     }
 }
--- QuantityMeasurementApp/src/com/apps/quantitymeasurement/LengthTest.java
+++ QuantityMeasurementApp/src/com/apps/quantitymeasurement/LengthTest.java
@@ -6,43 +6,42 @@ import static org.junit.Assert.*;
 public class LengthTest {
 
     @Test
-    public void testFeetToFeetSameValue() {
-        assertTrue(new Length(1.0, LengthUnit.FEET)
-                .equals(new Length(1.0, LengthUnit.FEET)));
-    }
-
-    @Test
-    public void testInchToInchSameValue() {
-        assertTrue(new Length(1.0, LengthUnit.INCHES)
-                .equals(new Length(1.0, LengthUnit.INCHES)));
-    }
-
-    @Test
     public void testFeetToInchEqual() {
-        assertTrue(new Length(1.0, LengthUnit.FEET)
-                .equals(new Length(12.0, LengthUnit.INCHES)));
+        assertTrue(new Length(1, LengthUnit.FEET)
+                .equals(new Length(12, LengthUnit.INCHES)));
     }
 
     @Test
-    public void testInchToFeetEqual() {
-        assertTrue(new Length(12.0, LengthUnit.INCHES)
-                .equals(new Length(1.0, LengthUnit.FEET)));
+    public void testAddition() {
+        Length l1 = new Length(1, LengthUnit.FEET);
+        Length l2 = new Length(12, LengthUnit.INCHES);
+
+        Length result = l1.add(l2);
+
+        assertTrue(result.equals(new Length(2, LengthUnit.FEET)));
     }
 
     @Test
-    public void testDifferentValues() {
-        assertFalse(new Length(1.0, LengthUnit.FEET)
-                .equals(new Length(2.0, LengthUnit.FEET)));
+    public void testSubtraction() {
+        Length l1 = new Length(2, LengthUnit.FEET);
+        Length l2 = new Length(12, LengthUnit.INCHES);
+
+        Length result = l1.subtract(l2);
+
+        assertTrue(result.equals(new Length(1, LengthUnit.FEET)));
     }
 
     @Test
-    public void testSameReference() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        assertTrue(l1.equals(l1));
+    public void testDivision() {
+        Length l1 = new Length(2, LengthUnit.FEET);
+        Length l2 = new Length(1, LengthUnit.FEET);
+
+        assertEquals(2.0, l1.divide(l2), 0.0);
     }
 
-    @Test
-    public void testNullComparison() {
-        assertFalse(new Length(1.0, LengthUnit.FEET).equals(null));
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        new Length(2, LengthUnit.FEET)
+                .divide(new Length(0, LengthUnit.FEET));
     }
 }
